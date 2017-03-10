2f2b70407998d07c97132e0571e799c6bbeb5469
Vincent Agnano <vincent.agnano@scopyleft.fr>
2017-03-10 16:14:51 +0100

    replaced config exports to a global default module

--- js/config.js
+++ js/config.js
@@ -1,79 +1,83 @@
-/**
- * The current user data
- */
-export const user = {
-  id: 1,
-  slug: 'first-last',
-  first_name: 'first',
-  last_name: 'last',
-  roles: ['admin'],
+module.exports = {
+  /**
+  * The current user data
+  */
+  user: {
+    id: 1,
+    slug: 'first-last',
+    first_name: 'first',
+    last_name: 'last',
+    roles: ['admin'],
+  },
+
+  /**
+   * Map debug features on Webpack DEBUG flag
+   */
+  debug: DEBUG,
+
+  /**
+   * The current language is guessed from the html lang attribute
+   */
+  lang: 'en',
+
+  /**
+   * Reuse server side site title
+   */
+  title: 'Admin - Udata',
+
+  /**
+   * The API Swagger specifications URL
+   */
+  api_specs: 'https://next.data.gouv.fr/api/1/swagger.json',
+
+  /**
+   * The theme static root URL
+   */
+  theme_static: '/',
+
+  /**
+   * The static assets root URL
+   */
+  static_root: '/',
+
+  /**
+   * The administration root URL
+   */
+  admin_root: '/en/admin',
+
+  /**
+   * The authentification URL for logins
+   */
+  auth_url: '/en/login',
+
+  /**
+   * Whether the 'delete me' feature is enabled or not.
+   */
+  is_delete_me_enabled: true,
+
+  /**
+   * Detect HiDPI screens
+   */
+  hidpi: (window.devicePixelRatio > 1 || (
+      window.matchMedia &&
+      window.matchMedia('(-webkit-min-device-pixel-ratio: 1.25),(min-resolution: 120dpi)').matches)
+  ),
+
+  /**
+   * Attributions for map tiles
+   */
+  tiles_attributions: '&copy;' + [
+      '<a href="http://openstreetmap.org/copyright">OpenStreetMap</a>',
+      '<a href="https://cartodb.com/attributions">CartoDB</a>'
+  ].join('/'),
+
+  /**
+   * Map tiles URL
+   */
+  tiles_url: `https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}${hidpi ? '@2x' : ''}.png`,
+
+  /**
+   * Leaflet base config
+   */
+  tiles_config: {subdomains: 'abcd', attribution: tiles_attributions},
 }
-
-/**
- * Map debug features on Webpack DEBUG flag
- */
-export const debug = DEBUG
-
-/**
- * The current language is guessed from the html lang attribute
- */
-export const lang = 'en'
-
-/**
- * Reuse server side site title
- */
-export const title = 'Admin - Udata'
-
-/**
- * The API Swagger specifications URL
- */
-export const api_specs = 'https://next.data.gouv.fr/api/1/swagger.json'
-
-/**
- * The theme static root URL
- */
-export const theme_static = '/'
-
-/**
- * The static assets root URL
- */
-export const static_root = '/'
-
-/**
- * The administration root URL
- */
-export const admin_root = '/en/admin'
-
-/**
- * The authentification URL for logins
- */
-export const auth_url = '/en/login'
-
-/**
- * Whether the 'delete me' feature is enabled or not.
- */
-export const is_delete_me_enabled = true
-
-/**
- * Detect HiDPI screens
- */
-export const hidpi = (window.devicePixelRatio > 1 || (
-    window.matchMedia
-    && window.matchMedia('(-webkit-min-device-pixel-ratio: 1.25),(min-resolution: 120dpi)').matches)
-)
-/**
- * Attributions for map tiles
- */
-export const tiles_attributions = '&copy;' + [
-    '<a href="http://openstreetmap.org/copyright">OpenStreetMap</a>',
-    '<a href="https://cartodb.com/attributions">CartoDB</a>'
-].join('/')
-/**
- * Map tiles URL
- */
-export const tiles_url = `https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}${hidpi ? '@2x' : ''}.png`
-
-/**
- * Leaflet base config
- */
-export const tiles_config = {subdomains: 'abcd', attribution: tiles_attributions}

55cc696ffd09096e19317323053949ed805a914b
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2017-03-12 21:00:26 +0100

    proxied swagger definition

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const webpack = require('webpack')
 const webpackMiddleware = require('webpack-dev-middleware')
 const httpProxy = require('http-proxy')
+const request = require('superagent')
 const webpackConfig = require('./webpack.config')
 const config = require('./config.json')
 
@@ -12,6 +13,18 @@ proxy.on('proxyReq', (proxyReq) => proxyReq.setHeader('X-API-KEY', config.API_KE
 
 const app = express()
 
+app.get('/api/1/swagger.json', (req, res, next) => {
+  request(config.API_ROOT_URL + '/swagger.json')
+    .then(resp => {
+      if (!resp.ok || !resp.body || !resp.body.definitions) {
+        throw new Error('Bad swagger definition')
+      }
+      const swagger = resp.body
+      swagger.host = undefined
+      res.send(swagger)
+    })
+    .catch(next)
+})
 app.use('/api/1', (req, res) => proxy.web(req, res, { changeOrigin: true, target: config.API_ROOT_URL }))
 
 app.use(webpackMiddleware(webpack(webpackConfig), {
